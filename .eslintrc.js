23e03fc2ac9f8b1fb6b875d1f879d1220bcc3a2e
paalamugan <paalamugan@gmail.com>
2022-01-27 11:16:56 +0530

    Fixed known issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,55 +1,55 @@
 module.exports = {
-    "parser": "babel-eslint",
+    "parser": `babel-eslint`,
     "parserOptions": {
         "ecmaVersion": 6,
         "ecmaFeatures": {
             "jsx": true,
-            "experimentalObjectRestSpread": true
-        }
+            "experimentalObjectRestSpread": true,
+        },
     },
-    plugins: ["react"],
+    plugins: [`react`],
     extends: [
-        "plugin:react/recommended"
+        `plugin:react/recommended`,
     ],
     "settings": {
         "react": {
-            "createClass": "createReactClass",
-            "pragma": "React",
-            "version": "detect",
-            "flowVersion": "0.53"
+            "createClass": `createReactClass`,
+            "pragma": `React`,
+            "version": `detect`,
+            "flowVersion": `0.53`,
         },
-        "propWrapperFunctions": [ "forbidExtraProps" ]
+        "propWrapperFunctions": [ `forbidExtraProps` ],
     },
     "rules": {
-        "no-console": "off",
-        "no-inner-declarations": "off",
-        "valid-jsdoc": "off",
-        "require-jsdoc": "off",
-        "quotes": ["error", "backtick"],
-        "consistent-return": ["error"],
+        "no-console": `off`,
+        "no-inner-declarations": `off`,
+        "valid-jsdoc": `off`,
+        "require-jsdoc": `off`,
+        "quotes": [`off`],
+        "consistent-return": [`error`],
         "arrow-body-style": [
-            "error",
-            "as-needed",
-            { "requireReturnForObjectLiteral": true }
+            `error`,
+            `as-needed`,
+            { "requireReturnForObjectLiteral": true },
         ],
-        "jsx-quotes": ["error", "prefer-double"],
-        "semi": ["off", "never"],
-        "object-curly-spacing": ["error", "always"],
+        "jsx-quotes": [`error`, `prefer-double`],
+        "semi": [`off`, `never`],
+        "object-curly-spacing": [`error`, `always`],
         "comma-dangle": [
-            "error",
+            `error`,
             {
-                "arrays": "always-multiline",
-                "objects": "always-multiline",
-                "imports": "always-multiline",
-                "exports": "always-multiline",
-                "functions": "ignore"
-            }
+                "arrays": `always-multiline`,
+                "objects": `always-multiline`,
+                "imports": `always-multiline`,
+                "exports": `always-multiline`,
+                "functions": `ignore`,
+            },
         ],
         "react/prop-types": [
-            "error",
+            `error`,
             {
-                "ignore": ["children"]
-            }
-        ]
-    }
+                "ignore": [`children`],
+            },
+        ],
+    },
 };
