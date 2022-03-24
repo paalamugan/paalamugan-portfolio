e242b70443f2d8ba71dc0dafd71622d051e9f6a9
paalamugan <paalamugan@gmail.com>
2022-03-24 16:49:18 +0530

    Minor fix

--- src/components/Common/Icon.js
+++ src/components/Common/Icon.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Img from "gatsby-image"
+// import Img from "gatsby-image"
 
 const Icon = ({ name, alt, className }) => {
 
@@ -12,11 +12,15 @@ const Icon = ({ name, alt, className }) => {
     name = arr[0];
     let isSvgExt = (ext === `svg`);
 
-    let imagePath = `../../assets/images${ isSvgExt ? `/icons/` : `/` }${name}.${ext}`;
-    let Image = require(imagePath) // name should with image extension
+    // let imagePath = `../../assets/images${ isSvgExt ? `/icons/` : `/` }${name}.${ext}`;
+    // let Image = require(imagePath) // name should with image extension
+
+    // return (isSvgExt ? <Image className={className} data-cy={`${name}-icon`}/> :
+    //                    <Img className={className} src={imagePath} alt={alt || name}/>);
+    let Image = require(`../../assets/images${ isSvgExt ? `/icons/` : `/` }${name}.${ext}`) // name should with image extension
 
     return (isSvgExt ? <Image className={className} data-cy={`${name}-icon`}/> :
-                       <Img className={className} src={imagePath} alt={alt || name}/>);
+                       <img className={className} src={Image.default} alt={alt || name}/>);
 
 }
 
