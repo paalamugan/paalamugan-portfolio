f065ae1f8816dc831bdb89ddbe7ba1bf77562a8a
paalamugan <paalamugan@gmail.com>
2022-05-02 00:31:34 +0530

    Added favicon.ico

--- src/components/Common/Meta/ImageMeta.js
+++ src/components/Common/Meta/ImageMeta.js
@@ -10,6 +10,12 @@ const ImageMeta = ({ image }) => (
         {/* These are the image sizes of the SEO feature images */}
         <meta property="og:image:width" content="1000" />
         <meta property="og:image:height" content="523" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
     </Helmet >
 )
 
